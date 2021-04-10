b389a6845056e1fe66ed421e79deed51c9726f38
Yaseen Kola <yaseenkola@gmail.com>
2021-04-10 11:37:10 +0400

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,6 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Backend\RingtoneController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-
 
 
 Auth::routes([
@@ -35,5 +17,5 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 // Route::resource('/ringtones', RingtoneController::class)->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::resource('ringtones', 'RingtoneController');
+    Route::resource('ringtones', RingtoneController::class);
 });
